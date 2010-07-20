26f5f3f3e0d96422dd567d0f50aee19b14182803
victor.kareh <victor.kareh@5b07db7b-0566-4881-90b5-aa5421b0cea3>
2010-07-20 17:29:07 +0000

    Case #2636: Multiple Creators in Copyright Text.
    git-svn-id: svn+ssh://dev.switchbackcms.com/var/svn/oer@178 5b07db7b-0566-4881-90b5-aa5421b0cea3

--- sites/all/modules/openmichigan/course.js
+++ sites/all/modules/openmichigan/course.js
@@ -19,10 +19,30 @@ if (Drupal.jsEnabled) {
       $("#edit-title").trigger('change');
     });
     $("#edit-field-creators-0-value").blur(function() { course_populate_author(); });
+    $("#edit-field-creators-1-value").blur(function() { course_populate_author(); });
+    $("#edit-field-creators-2-value").blur(function() { course_populate_author(); });
+    $("#edit-field-creators-3-value").blur(function() { course_populate_author(); });
+    $("#edit-field-creators-4-value").blur(function() { course_populate_author(); });
+    $("#edit-field-creators-5-value").blur(function() { course_populate_author(); });
+    $("#edit-field-creators-6-value").blur(function() { course_populate_author(); });
+    $("#edit-field-creators-7-value").blur(function() { course_populate_author(); });
+    $("#edit-field-creators-8-value").blur(function() { course_populate_author(); });
+    $("#edit-field-creators-9-value").blur(function() { course_populate_author(); });
   });
 }
 
 function course_populate_author() {
-  $("#edit-creativecommons-metadata-attributionName").val($("#edit-field-creators-0-value").val());
-  $("#edit-creativecommons-metadata-creator").val($("#edit-field-creators-0-value").val());
+  var author = $("#edit-field-creators-0-value").val();
+  if ($("#edit-field-creators-1-value").val()) { author = author + ', ' + $("#edit-field-creators-1-value").val(); }
+  if ($("#edit-field-creators-2-value").val()) { author = author + ', ' + $("#edit-field-creators-2-value").val(); }
+  if ($("#edit-field-creators-3-value").val()) { author = author + ', ' + $("#edit-field-creators-3-value").val(); }
+  if ($("#edit-field-creators-4-value").val()) { author = author + ', ' + $("#edit-field-creators-4-value").val(); }
+  if ($("#edit-field-creators-5-value").val()) { author = author + ', ' + $("#edit-field-creators-5-value").val(); }
+  if ($("#edit-field-creators-6-value").val()) { author = author + ', ' + $("#edit-field-creators-6-value").val(); }
+  if ($("#edit-field-creators-7-value").val()) { author = author + ', ' + $("#edit-field-creators-7-value").val(); }
+  if ($("#edit-field-creators-8-value").val()) { author = author + ', ' + $("#edit-field-creators-8-value").val(); }
+  if ($("#edit-field-creators-9-value").val()) { author = author + ', ' + $("#edit-field-creators-9-value").val(); }
+  $("#edit-creativecommons-metadata-attributionName").val(author);
+  $("#edit-creativecommons-metadata-creator").val(author);
 }
+
