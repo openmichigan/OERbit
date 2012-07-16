20bc1d03cf1163ac2b9150eadd0799ce20622df7
Kevin Coffman <kwc@umich.edu>
2012-07-16 09:04:27 -0400

    Allow multiple video links for a material
    - Pull in changes/fixes for the Internet Archive and
      Slideshare emvideo providers.
    - Modify the view.
    - Modify the material fields to allow multiple video links
    - Modify the theming, adding icons for various services.
    - Modify the material JavaScript file to never hide the video links.

--- sites/all/modules/openmichigan/material.js
+++ sites/all/modules/openmichigan/material.js
@@ -46,9 +46,9 @@ function material_resource_type() {
   $("#field-file-items").hide();
   $("#edit-field-image-0-upload").attr("disabled", "disabled");
   $("#edit-field-image-0-wrapper").hide();
-  $("#edit-field-video-0-embed").attr("disabled", "disabled");
-  $("#edit-field-video-0-embed-wrapper").hide();
-  $("#edit-field-video-0-embed-wrapper + .form-item").hide();
+  //$("#edit-field-video-0-embed").attr("disabled", "disabled");
+  //$("#edit-field-video-0-embed-wrapper").hide();
+  //$("#edit-field-video-0-embed-wrapper + .form-item").hide();
   $("#edit-field-link-0-url").attr("disabled", "disabled");
   $("#edit-field-link-0-url-wrapper").hide();
   switch ($("#edit-field-resource-type-value").val()) {
@@ -60,11 +60,11 @@ function material_resource_type() {
       $("#edit-field-image-0-upload").removeAttr("disabled");
       $("#edit-field-image-0-wrapper").show();
       break;
-    case 'video':
-      $("#edit-field-video-0-embed").removeAttr("disabled");
-      $("#edit-field-video-0-embed-wrapper").show();
-      $("#edit-field-video-0-embed-wrapper + .form-item").show();
-      break;
+    //case 'video':
+      //$("#edit-field-video-0-embed").removeAttr("disabled");
+      //$("#edit-field-video-0-embed-wrapper").show();
+      //$("#edit-field-video-0-embed-wrapper + .form-item").show();
+      //break;
     case 'link':
       $("#edit-field-link-0-url").removeAttr("disabled");
       $("#edit-field-link-0-url-wrapper").show();
